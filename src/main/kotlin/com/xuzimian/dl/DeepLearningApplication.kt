11abe3bb08c17f56d556b414a221bca302708e11
xuzimian <516654313@qq.com>
2025-04-09 11:30:34 +0800

    初始化

---
+++ src/main/kotlin/com/xuzimian/dl/DeepLearningApplication.kt
@@ -0,0 +1,11 @@
+package com.xuzimian.dl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeepLearningApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeepLearningApplication>(*args)
+}
---
+++ src/test/kotlin/com/xuzimian/dl/DeepLearningApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xuzimian.dl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeepLearningApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
