36bbecb853ca9cb9a3bec37e280af15eb1adabc1
xuzimian <516654313@qq.com>
2025-04-11 23:41:09 +0800

    add a new toBufferedImage method

--- src/main/kotlin/com/xuzimian/dl/utils/MatExtension.kt
+++ src/main/kotlin/com/xuzimian/dl/utils/MatExtension.kt
@@ -1,6 +1,7 @@
 package com.xuzimian.dl.utils
 
 import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
 import java.awt.image.BufferedImage
 import java.awt.image.DataBufferByte
 
@@ -17,4 +18,24 @@ fun Mat.toBufferedImage(): BufferedImage {
     val targetPixels = (image.raster.dataBuffer as DataBufferByte).data
     System.arraycopy(buffer, 0, targetPixels, 0, buffer.size)
     return image
+}
+
+
+fun toBufferedImage(mat: Mat): BufferedImage {
+    val type =
+        if (mat.channels() != 1) BufferedImage.TYPE_3BYTE_BGR else BufferedImage.TYPE_BYTE_GRAY
+
+    if (type == BufferedImage.TYPE_3BYTE_BGR) {
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_BGR2RGB)
+    }
+
+    val width = mat.width()
+    val height = mat.height()
+    val data = ByteArray(width * height * mat.elemSize().toInt())
+    mat[0, 0, data]
+
+    val ret = BufferedImage(width, height, type)
+    ret.raster.setDataElements(0, 0, width, height, data)
+
+    return ret
 }
